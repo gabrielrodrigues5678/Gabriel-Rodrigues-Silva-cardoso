225605cec5bf6ea8f19cf6fbdf2d759a3d15b15a
gabrielrodrigues5678 <63113861+gabrielrodrigues5678@users.noreply.github.com>
2020-05-31 16:30:23 -0300

    1004 by Daniel Rodrigues Silva Cardoso

---
+++ SimpleProduct.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+      int n1;
+      int n2;
+      
+      Scanner sc = new Scanner(System.in);
+      
+      
+      System.out.println("Digite o primeiro numero ");
+      n1 = sc.nextInt();
+      
+      System.out.println("Digite o segundo numero ");
+      n2 = sc.nextInt();
+    
+    System.out.print("PROD = " + (n1 * n2));
+        
+    }
+    }
